cc035fe64e42a43e8ab95d572c14b14c31264e33
MeanEYE.rcf <MeanEYE.rcf@localhost>
2011-06-26 16:09:59 +0200

    History list: Moved to GI model.

--- application/gui/history_list.py
+++ application/gui/history_list.py
@@ -1,71 +1,74 @@
 import os
-import gtk
+
+from gi.repository import GObject, Gtk, Gdk
 
 COL_NAME		= 0
 COL_PATH		= 1
 COL_TIMESTAMP	= 2
 
 
-class HistoryList(gtk.Window):
+class HistoryList(GObject.GObject):
 	"""History list is used to display complete browsing history."""
 
 	def __init__(self, parent, application):
 		# create main window
-		gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+		super(HistoryList, self).__init__()
+		self.window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
 
 		# store parameters locally, we'll need them later
 		self._parent = parent
 		self._application = application
 
 		# configure dialog
-		self.set_title(_('History'))
-		self.set_size_request(500, 300)
-		self.set_resizable(True)
-		self.set_skip_taskbar_hint(True)
-		self.set_modal(True)
-		self.set_transient_for(application)
-		self.set_wmclass('Sunflower', 'Sunflower')
-		self.set_border_width(7)
+		self.window.set_title(_('History'))
+		self.window.set_size_request(500, 300)
+		self.window.set_position(Gtk.WindowPosition.CENTER_ON_PARENT)
+		self.window.set_resizable(True)
+		self.window.set_skip_taskbar_hint(True)
+		self.window.set_modal(True)
+		self.window.set_transient_for(self._application.window)
+		self.window.set_wmclass('Sunflower', 'Sunflower')
+		self.window.set_border_width(7)
 
 		# create UI
-		vbox = gtk.VBox(False, 7)
+		vbox = Gtk.VBox(False, 7)
 
-		list_container = gtk.ScrolledWindow()
-		list_container.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-		list_container.set_shadow_type(gtk.SHADOW_IN)
+		list_container = Gtk.ScrolledWindow()
+		list_container.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+		list_container.set_shadow_type(Gtk.ShadowType.IN)
 
-		self._history = gtk.ListStore(str, str)
+		self._history = Gtk.ListStore(str, str)
 
-		cell_name = gtk.CellRendererText()
-		cell_path = gtk.CellRendererText()
+		cell_name = Gtk.CellRendererText()
+		cell_path = Gtk.CellRendererText()
 
-		col_name = gtk.TreeViewColumn(_('Name'), cell_name, text=COL_NAME)
-		col_path = gtk.TreeViewColumn(_('Path'), cell_path, text=COL_PATH)
+		col_name = Gtk.TreeViewColumn(_('Name'), cell_name, text=COL_NAME)
+		col_path = Gtk.TreeViewColumn(_('Path'), cell_path, text=COL_PATH)
 
-		self._history_list = gtk.TreeView(self._history)
+		self._history_list = Gtk.TreeView(self._history)
 		self._history_list.connect('key-press-event', self._handle_key_press)
 		self._history_list.append_column(col_name)
 		self._history_list.append_column(col_path)
 
 		# create controls
-		hbox_controls = gtk.HBox(False, 5)
+		hbox_controls = Gtk.HBox(False, 5)
 
-		btn_close = gtk.Button(stock=gtk.STOCK_CLOSE)
+		btn_close = Gtk.Button(stock=Gtk.STOCK_CLOSE)
 		btn_close.connect('clicked', self._close)
 
-		image_jump = gtk.Image()
-		image_jump.set_from_icon_name('go-jump', gtk.ICON_SIZE_BUTTON)
+		image_jump = Gtk.Image()
+		image_jump.set_from_icon_name('go-jump', Gtk.IconSize.BUTTON)
 
-		btn_jump = gtk.Button()
+		btn_jump = Gtk.Button()
 		btn_jump.set_image(image_jump)
 		btn_jump.set_label(_('Go to'))
 		btn_jump.set_can_default(True)
 		btn_jump.connect('clicked', self._change_path)
 
-		image_new_tab = gtk.Image()
-		image_new_tab.set_from_icon_name('tab-new', gtk.ICON_SIZE_BUTTON)
+		image_new_tab = Gtk.Image()
+		image_new_tab.set_from_icon_name('tab-new', Gtk.IconSize.BUTTON)
 
-		btn_new_tab = gtk.Button()
+		btn_new_tab = Gtk.Button()
 		btn_new_tab.set_image(image_new_tab)
 		btn_new_tab.set_label(_('New tab'))
 		btn_new_tab.set_tooltip_text(_('Open selected path in new tab'))
@@ -81,17 +84,17 @@ class HistoryList(gtk.Window):
 		vbox.pack_start(list_container, True, True, 0)
 		vbox.pack_start(hbox_controls, False, False, 0)
 
-		self.add(vbox)
+		self.window.add(vbox)
 
 		# populate history list
 		self._populate_list()
 
 		# show all elements
-		self.show_all()
+		self.window.show_all()
 
 	def _close(self, widget=None, data=None):
 		"""Handle clicking on close button"""
-		self.destroy()
+		self.window.destroy()
 
 	def _change_path(self, widget=None, data=None):
 		"""Change to selected path"""
@@ -130,10 +133,10 @@ class HistoryList(gtk.Window):
 	def _handle_key_press(self, widget, event, data=None):
 		"""Handle pressing keys in history list"""
 		result = False
-		key_name = gtk.gdk.keyval_name(event.keyval)
+		key_name = Gdk.keyval_name(event.keyval)
 
 		if key_name == 'Return':
-			if event.state & gtk.gdk.CONTROL_MASK:
+			if event.state & Gdk.CONTROL_MASK:
 				# open path in new tab
 				self._open_in_new_tab()
 
