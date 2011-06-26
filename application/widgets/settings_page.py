b552213be578c8cd47230d563a3e347b4f328777
MeanEYE.rcf <MeanEYE.rcf@localhost>
2011-06-26 21:11:31 +0200

    Settings page: Changed class parent class.

--- application/widgets/settings_page.py
+++ application/widgets/settings_page.py
@@ -1,11 +1,15 @@
-from gi.repository import Gtk
+from gi.repository import GObject, Gtk
 
 
-class SettingsPage(Gtk.VBox):
+class SettingsPage(GObject.GObject):
 	"""Abstract class used to build pages in preferences window."""
 	
+	__gtype_name__ = 'Sunflower_Settings_page'
+	
 	def __init__(self, parent, application, name, title):
-		super(SettingsPage, self).__init__(False, 0)
+		super(SettingsPage, self).__init__()
+		
+		self.vbox = Gtk.VBox(homogeneous=False, spacing=0)
 
 		self._parent = parent
 		self._application = application
@@ -13,11 +17,11 @@ class SettingsPage(Gtk.VBox):
 		self._page_title = title
 
 		# configure self
-		self.set_spacing(5)
-		self.set_border_width(0)
+		self.vbox.set_spacing(5)
+		self.vbox.set_border_width(0)
 		
 		# add page to preferences window
-		self._parent.add_tab(self._page_name, self._page_title, self)
+		self._parent.add_tab(self._page_name, self._page_title, self.vbox)
 		
 	def _load_options(self):
 		"""Load options and update interface"""
