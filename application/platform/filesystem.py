9ffa52643f11fdef75816a28e069276eba36bccc
Grigory Petrov <grigory.v.p@gmail.com>
2011-10-01 12:24:18 +0300

    gnomevfs is not available in ubuntu 11.10 beta 2 - implemented fallback to default

--- application/platform/filesystem.py
+++ application/platform/filesystem.py
@@ -4,35 +4,49 @@ import sys
 import os
 import mimetypes
 
+def d_mime_get_all_applications(mime_type):
+	pass
+
+def d_mime_get_default_application(mime_type):
+	pass
+
+def d_get_mime_type(path):
+	if not mimetypes.inited:
+		mimetypes.init()
+	type = mimetypes.guess_type(path)[ 0 ]
+	if type:
+		return type
+	if os.path.isdir( path ):
+		return 'x-directory/normal'
+	return 'text/plain'
+
+def d_is_executable_command_string(path):
+	pass
+
+def d_mime_get_description(mime_type):
+	pass
+
 if sys.platform == 'linux2':
 	# import linux modules
-	from gnomevfs import *
+	try:
+		from gnomevfs import *
+	except:
+		mime_get_all_applications = d_mime_get_all_applications
+		mime_get_default_application = d_mime_get_default_application
+		get_mime_type = d_get_mime_type
+		is_executable_command_string = d_is_executable_command_string
+		mime_get_description = d_mime_get_description
+
 	from os import statvfs
 
 elif sys.platform == 'win32':
 	# define methods for Windows
 
-	def mime_get_all_applications(mime_type):
-		pass
-
-	def mime_get_default_application(mime_type):
-		pass
-
-	def get_mime_type(path):
-		if not mimetypes.inited:
-			mimetypes.init()
-		type = mimetypes.guess_type(path)[ 0 ]
-		if type:
-			return type
-		if os.path.isdir( path ):
-			return 'x-directory/normal'
-		return 'text/plain'
-
-	def is_executable_command_string(path):
-		pass
-
-	def mime_get_description(mime_type):
-		pass
+	mime_get_all_applications = d_mime_get_all_applications
+	mime_get_default_application = d_mime_get_default_application
+	get_mime_type = d_get_mime_type
+	is_executable_command_string = d_is_executable_command_string
+	mime_get_description = d_mime_get_description
 
 	def statvfs(path):
 		class Stat(object):
@@ -45,27 +59,11 @@ elif sys.platform == 'win32':
 elif sys.platform == 'darwin':
 	# define methods for OS/X
 
-	def mime_get_all_applications(mime_type):
-		pass
-
-	def mime_get_default_application(mime_type):
-		pass
-
-	def get_mime_type(path):
-		if not mimetypes.inited:
-			mimetypes.init()
-		type = mimetypes.guess_type(path)[ 0 ]
-		if type:
-			return type
-		if os.path.isdir( path ):
-			return 'x-directory/normal'
-		return 'text/plain'
-
-	def is_executable_command_string(path):
-		pass
-
-	def mime_get_description(mime_type):
-		pass
+	mime_get_all_applications = d_mime_get_all_applications
+	mime_get_default_application = d_mime_get_default_application
+	get_mime_type = d_get_mime_type
+	is_executable_command_string = d_is_executable_command_string
+	mime_get_description = d_mime_get_description
 
 	from os import statvfs
 
