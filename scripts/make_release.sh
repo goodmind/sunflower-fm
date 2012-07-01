660b18d63bf912b8b34e5c475bacdbc052f5a3a4
Wojciech Kluczka <wojtekkluczka@gmail.com>
2012-07-01 20:00:20 +0200

    Sessions plugin: further corrections

---
+++ scripts/make_release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+working_directory=`hg root`
+version_major=`cat $working_directory/application/gui/main_window.py | grep \'major\': | cut -f 2 -d : | tr -d [:space:][,]`
+version_minor=`cat $working_directory/application/gui/main_window.py | grep \'minor\': | cut -f 2 -d : | tr -d [:space:][,]`
+version_build=`cat $working_directory/application/gui/main_window.py | grep \'build\': | cut -f 2 -d : | tr -d [:space:][,]`
+version_stage=`cat $working_directory/application/gui/main_window.py | grep \'stage\': | cut -f 2 -d : | tr -d [:space:][\'][,]`
+
+# form file names
+file_google_code="Sunflower-$version_major.$version_minor$version_stage-$version_build.tgz"
+file_home_page="Sunflower.tgz"
+tar_file_home_page="Sunflower.tar"
+
+# archive files
+echo Preparing release: Sunflower $version_major.$version_minor$version_stage \($version_build\)
+echo -e "\t- Archiving source code"
+cd $working_directory
+hg archive --exclude scripts/ --type tgz --prefix Sunflower ~/Desktop/$file_home_page
+
+# remove unneeded files
+echo -e "\t- Unpacking gzip archive"
+gunzip ~/Desktop/$file_home_page
+
+echo -e "\t- Removing unneeded files"
+tar --delete --wildcards --file=$HOME/Desktop/$tar_file_home_page Sunflower/.hg*
+tar --delete --wildcards --file=$HOME/Desktop/$tar_file_home_page Sunflower/images/*.xcf
+tar --delete --wildcards --file=$HOME/Desktop/$tar_file_home_page Sunflower/images/selection_arrow.svg
+
+echo -e "\t- Repacking gzip archive"
+gzip --best ~/Desktop/$tar_file_home_page
+
+# create a duplicate
+echo -e "\t- Duplicating file"
+mv ~/Desktop/$tar_file_home_page.gz ~/Desktop/$file_home_page
+cp ~/Desktop/$file_home_page ~/Desktop/$file_google_code
+
+echo "Done!"
